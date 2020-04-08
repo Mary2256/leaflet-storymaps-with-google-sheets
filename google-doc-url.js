022841b4153d2461a4a38941d7ecf82e7fbbd871
Mary2256 <63361981+Mary2256@users.noreply.github.com>
2020-04-08 21:42:26 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UVKiNMZeYVwY7HCf0xAg2j0rub3qoQNSZUQc0BMULJo/edit#gid=0';
